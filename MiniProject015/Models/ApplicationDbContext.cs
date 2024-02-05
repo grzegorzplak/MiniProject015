f5c72ead847e990f182dc8013bdd7f9e5c7a5f34
Grzegorz <Grzegorz@LAPTOP-2JK6GIJ7>
2024-02-05 16:54:13 +0100

    Add project files.

---
+++ MiniProject015/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
+
+namespace MiniProject015.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseMySQL("server=localhost;database=MiniProject015;uid=root;pwd=");
+        }
+    }
+}
---
+++ MiniProject015/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniProject015.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range(0, 1000)]
+        public decimal Price { get; set; }
+    }
+}
